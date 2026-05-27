357cc792533c5f57da12c2298b686b8373714219
sallliisa <sodiumcation@gmail.com>
2026-05-27 23:24:54 +0700

    gamer

--- apps/landing-admin-web/src/views/authenticated/companyProfile/companyProfile/companyProfile.vue
+++ apps/landing-admin-web/src/views/authenticated/companyProfile/companyProfile/companyProfile.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
-import CRUDComposite from '@southneuhof/is-vue-framework/components/composites/CRUDComposite.vue'
+import Card from '@southneuhof/is-vue-framework/components/base/Card.vue'
+import Form from '@southneuhof/is-vue-framework/components/composites/Form.vue'
+import { toast } from 'vue-sonner'
 import companyProfileModel from '@client/data-model/models/companyProfile.model'
+import { buildFormConfig } from '@southneuhof/is-data-model'
+
+const updateConfig = buildFormConfig(companyProfileModel, 'update')
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <CRUDComposite :config="companyProfileModel" />
+    <Card>
+      <Form
+        :fields="['name', 'address', 'email', 'phone', 'facebook', 'instagram', 'youtube']"
+        :targetAPI="updateConfig.targetAPI"
+        :getAPI="companyProfileModel.name"
+        dataID="1"
+        :fieldsAlias="updateConfig.fieldsAlias"
+        :inputConfig="updateConfig.inputConfig"
+        formType="update"
+        :onSuccess="
+          () => {
+            toast.success('Berhasil menyimpan company profile!')
+          }
+        "
+      />
+    </Card>
   </div>
 </template>
